616ff27145e17c7b8c25328ee2907e0824d633ab
Jason Edmund Schubert <j@smail.co>
2026-04-08 14:27:16 -0400

    Initial Commit

---
+++ SportsGPT/SportsGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportsGPTApp.swift
+//  SportsGPT
+//
+//  Created by Jason Schubert on 4/8/26.
+//
+
+import SwiftUI
+
+@main
+struct SportsGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
